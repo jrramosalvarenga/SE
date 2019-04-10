89c8b341d753962a39e19f33ac0cfa9962b6c9f2
jrramosalvarenga <juanrra2013@gmail.com>
2019-04-10 15:02:02 -0600

    Segundo commit vistas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,13 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Juan Ra
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://laravel.com/docs">Documentos</a>
+                    <a href="https://laracasts.com">Entrevistas</a>
+                    <a href="https://laravel-news.com">Noticias</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
